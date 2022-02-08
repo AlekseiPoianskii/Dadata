8f8b31d13fab52c0afd36380d58bf9b5adb17d53
polianskiialeksei <polianskiialeksei@gmail.com>
2022-02-09 02:39:46 +0700

    add logic and message

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tools import *
 if __name__ == "__main__":
     connection = sqlite3.connect("dadata.db")
     cursor = connection.cursor()
-    token = start_program(connection, cursor)
-    session = Dadata(token)
-    search_geo(session)
+    user = start_program(connection, cursor)
+    session = Dadata(user[1])
+    search_geo(session, user)
+    print(END_MESSAGE)
